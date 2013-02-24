a9935de206f7282956640b70ec227f8317758376
Lessandro Mariano <lessandro@gmail.com>
2013-02-24 15:47:46 -0300

    Add socket.io test client

--- start_socketio.py
+++ start_socketio.py
@@ -6,6 +6,6 @@ from servers.sioserver import SioServer
 import config
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.INFO)
     server = SioServer(config)
     tornado.ioloop.IOLoop.instance().start()
---
+++ tests/socketio.js
@@ -0,0 +1,31 @@
+var io = require('socket.io-client');
+
+var s = new io.connect('http://localhost:8001', {
+    rememberTransport: false
+});
+
+function send(data) {
+    s.send(data + "\r\n");
+}
+
+s.on('connect', function() {
+    console.log('connected');
+    send('hello');
+});
+
+s.on('message', function(data) {
+    console.log('message ' + data.trim());
+});
+
+s.on('disconnect', function() {
+    console.log('disconnected');
+});
+
+var i = 0;
+var x = setInterval(function() {
+    send("u craz " + i);
+    if (i++ == 4) {
+        s.disconnect();
+        clearInterval(x);
+    }
+}, 1000);
