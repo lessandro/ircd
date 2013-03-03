fed0137b865a86f61573da8331b3fe069678c0f0
Lessandro Mariano <lessandro@gmail.com>
2013-03-03 11:01:08 -0300

    Move nick/user to irc_auth.py

---
+++ kernel/irc_auth.py
@@ -0,0 +1,54 @@
+import re
+from command import command
+
+nick_re = re.compile(ur'^\w{3,}$', re.UNICODE)
+user_re = re.compile(r'^[a-zA-Z0-9_~]{1,}$')
+
+
+@command
+def cmd_nick(server, user, nick):
+    if user['nick'] != '*':
+        server.send_raw(user, '999', ':You may not change your nick')
+        return
+
+    nick_utf8 = nick.decode('utf-8')
+
+    if not nick_re.match(nick_utf8):
+        server.send_raw(user, '999', ':Invalid nick')
+        return
+
+    user['nick'] = nick
+    server.save_user(user)
+    check_auth(server, user)
+
+
+@command
+def cmd_user(server, user, username, hostname, servername, realname):
+    username = username[:10]
+
+    if not user_re.match(username):
+        server.send_raw(user, '999', ':Invalid username')
+        return
+
+    if 'username' in user:
+        server.send_raw(user, '462', ':You may not reregister')
+
+    user['username'] = username
+    server.save_user(user)
+    check_auth(server, user)
+
+
+def check_auth(server, user):
+    if 'auth' in user:
+        return
+
+    if 'username' not in user:
+        return
+
+    if user['nick'] == '*':
+        return
+
+    user['auth'] = True
+    user['id'] = '%s!%s@%s' % (user['username'], user['nick'], user['ip'])
+    server.save_user(user)
+    server.send_raw(user, '001', ':Welcome')
--- kernel/irc_basic.py
+++ kernel/irc_basic.py
@@ -1,46 +1,4 @@
-import re
 from command import command
-
-nick_re = re.compile(r'^\w{3,}$')
-
-
-@command
-def cmd_nick(server, user, nick):
-    if not nick_re.match(nick):
-        server.send_raw(user, '999', ':Invalid nick')
-        return
-
-    user['nick'] = nick
-    server.save_user(user)
-
-
-@command
-def cmd_user(server, user, username, hostname, servername, realname):
-    if not nick_re.match(username):
-        server.send_raw(user, '999', ':Invalid username')
-        return
-
-    if 'username' in user:
-        server.send_raw(user, '462', ':You may not reregister')
-
-    user['username'] = username
-    server.save_user(user)
-    check_auth(server, user)
-
-
-def check_auth(server, user):
-    if user['nick'] == '*':
-        return
-
-    if 'username' not in user:
-        return
-
-    if 'auth' in user:
-        return
-
-    user['auth'] = True
-    server.save_user(user)
-    server.send_raw(user, '001', ':Welcome')
 
 
 @command
