4441d0f2e783115408a730c15055c1deb73c3744
Lessandro Mariano <lessandro@gmail.com>
2013-03-13 08:59:21 -0300

    Msg tests

---
+++ ircd/tests/test_msg.py
@@ -0,0 +1,30 @@
+from testutil import *
+
+
+def test_privmsg(k1):
+    msg('JOIN #a')
+    assert code() == 'JOIN'
+
+    user(2)
+
+    msg('JOIN #a', 2)
+    assert code() == 'JOIN'
+
+    msg('PRIVMSG #a abc')
+    assert pop() == 'test:__2 :test1!test1@::1 PRIVMSG #a abc\r\n'
+
+    raw('connect test:__3 ::3')
+    msg('PRIVMSG #a zxc', 3)
+    assert code() == '451'  # not registered
+
+    msg('PRIVMSG #b qwe')
+    assert code() == '403'  # no such channel
+
+    msg('PRIVMSG #a')
+    assert code() == '461'  # not enough params
+
+    msg('PART #a')
+    assert code() == 'PART'
+
+    msg('PRIVMSG #a test test')
+    assert code() == '442'  # not on channel
