d56127f28bdc71c0db3b091b902f60551f19ec85
Lessandro Mariano <lessandro@gmail.com>
2013-03-13 08:59:29 -0300

    Kernel tests

---
+++ ircd/tests/test_kernel.py
@@ -0,0 +1,22 @@
+from testutil import *
+
+
+def test_reset(k1):
+    msg('JOIN #a')
+    pop()
+
+    raw('reset test ')
+    assert pop() == 'test:__1 :test1!test1@::1 PART #a\r\n'
+
+
+def test_utf8(k1):
+    msg('PING \xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA')
+    assert code() == '903'  # invalid utf8
+
+
+def test_nouser(k0):
+    msg('PING oi')
+    assert code() is None
+
+    raw('disconnect test:__1 ')
+    assert code() is None
