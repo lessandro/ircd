2d6f675ea4437ccaa2e6c8fccf779a97aedb8237
Lessandro Mariano <lessandro@gmail.com>
2013-05-27 12:03:13 -0300

    irc_mode: Add unit tests

---
+++ ircd/tests/test_mode.py
@@ -0,0 +1,60 @@
+from testutil import *
+
+
+def test_mode_chan(k1):
+    msg('JOIN #a')
+    user(2)
+    msg('JOIN #a', 2)
+    msg('JOIN #b', 2)
+
+    while pop():
+        pass
+
+    msg('MODE #c +o test')
+    assert code() == '403'  # no such channel
+
+    # not in channel
+    msg('MODE #b +o test')
+    assert code() == '482'  # not chan op
+
+    msg('MODE #a +o test9')
+    assert code() == '441'  # target not in chan
+
+    # in channel, not op
+    msg('MODE #a +o test2', 2)
+    assert code() == '482'  # not chan op
+
+    msg('MODE #a +y test1')
+    assert code() == '472'  # unknown mode
+
+    msg('MODE #a +o test2')
+    assert pop() == 'test:__1,test:__2 :test1!test1@::1 MODE #a +o test2\r\n'
+
+    # already op
+    msg('MODE #a +o test2')
+    assert not pop()
+
+    msg('MODE #a +v test2', 2)
+    assert pop() == 'test:__1,test:__2 :test2!test2@::2 MODE #a +v test2\r\n'
+
+    msg('MODE #a -o test2', 2)
+    assert pop() == 'test:__1,test:__2 :test2!test2@::2 MODE #a -o test2\r\n'
+
+    user(3)
+    while pop():
+        pass
+
+    msg('JOIN #a', 3)
+    pop()  # JOIN
+    pop()  # topic
+    names = pop().strip().split(':')[3].split(' ')
+    assert '@test1' in names
+    assert '+test2' in names
+    assert 'test3' in names
+    pop()  # end of names
+
+    msg('MODE #a +vv-v+v test1 test3 test3')
+    assert code() == 'MODE'
+    assert code() == 'MODE'
+    assert code() == 'MODE'
+    assert not pop()  # no target supplied for last +v
